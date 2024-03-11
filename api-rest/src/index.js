f7285a682ee49dacf63dfec627c220e632ff2e06
mauklo <maucalodolce20@gmail.com>
2024-03-11 17:29:51 -0300

    firstNode project

---
+++ api-rest/src/index.js
@@ -0,0 +1,12 @@
+//importamos la libreria
+const express = require("express");
+
+//creamos el proyecto express 
+const app = express();
+
+//levantamos la aplicacion
+app.listen(8080, ()=>{ 
+
+    console.log("App On!"); 
+
+});
